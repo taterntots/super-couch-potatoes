898190db66ded5bba92265c4f9fa11dbda922970
Matthew Weidner <matthew.a.weidner@gmail.com>
2021-12-30 11:28:04 -0800

    created a friend tracking app via a ruby on rails tutorial. will need lots of modifying for couch potatoes

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
+
+  resources :friends
+  get 'home/about'
+  # root 'home#index'
+  root 'friends#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
