e0954e7ba4f9e99401afa96441e9af22b6e38b86
Matthew Weidner <matthew.a.weidner@gmail.com>
2021-12-30 15:17:12 -0800

    removed all instances of the friends table and models

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
   end
   
   resources :media
-  resources :friends
   get 'home/about'
   root 'media#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -12,18 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2021_12_30_205323) do
 
-  create_table "friends", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "phone"
-    t.string "twitter"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
-    t.index ["user_id"], name: "index_friends_on_user_id"
-  end
-
   create_table "media", force: :cascade do |t|
     t.string "title"
     t.integer "rating"
